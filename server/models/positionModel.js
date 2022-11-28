501118f95507e3d16fa0c81f1809e95928cad0ff
Alvin-MFV <le.minh.an@moneyforward.vn>
2022-11-28 21:58:30 +0700

    Add position

---
+++ server/models/positionModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose")
+
+const positionSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    field: {
+      type: String,
+    },
+    salary:{
+      type: String,
+    },
+    type: {
+      type: String,
+    },
+    deadline: {
+      type: Date,
+    },
+    benefit: {
+      type: String,
+    },
+    required_skills: {
+      type: [String],
+    },
+    required_employees: {
+      type: Number,
+    },
+    company: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Company"
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+let Position = mongoose.models.position || mongoose.model("position", positionSchema)
+module.exports = Position
