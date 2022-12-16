b3e283085e44ab9dd8599eec94eabc26927043fa
ducpro100520 <62461886+ducpro100520@users.noreply.github.com>
2022-12-16 23:21:41 +0700

    add api change student infor and api get infor of job

---
+++ server/models/commonJobModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const commonJobSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    video_link: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+let commonJob = mongoose.models.commonJob || mongoose.model('commonJob', commonJobSchema)
+module.exports = commonJob
---
+++ server/routes/studentRoute.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const Student = require('../models/studentModel.js')
+
+const studentRouter = express.Router()
+
+const dotenv = require('dotenv')
+const auth = require('../auth.js')
+dotenv.config()
+
+studentRouter.get('/:id', async (req, res) => {
+  try {
+    const authResult = await auth(req, res)
+    if (authResult.role != 'student' && authResult.role != 'company') {
+      return res.status(403).send({ message: 'Bạn không có quyền' })
+    }
+
+    console.log(req.params.id)
+
+    const student = await Student.findOne({ userId: req.params.id }).populate('jobs')
+    if (!student) {
+      return res.status(404).send({ message: 'Không tìm thấy sinh viên' })
+    }
+
+    return res.json({
+      msg: 'Success',
+      data: student,
+    })
+  } catch (error) {
+    return res.status(500).json({ err: error.message })
+  }
+})
+
+studentRouter.put('/:id', async (req, res) => {
+  try {
+    const authResult = await auth(req, res)
+
+    if (authResult.role != 'student' || authResult.id != req.params.id) {
+      return res.status(403).send({ message: 'Bạn không có quyền' })
+    }
+
+    const student = await Student.findOneAndUpdate({ userId: req.params.id }, req.body, {
+      new: true,
+    })
+
+    return res.json({
+      msg: 'Success',
+      data: student,
+    })
+  } catch (error) {
+    return res.status(500).json({ err: error.message })
+  }
+})
+
+module.exports = studentRouter
