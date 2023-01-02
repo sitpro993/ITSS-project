afc1bb65fcbebf66480443dd02d6a6da231b14d8
Dang Dat <khacdatdang@gmail.com>
2023-01-02 16:49:34 +0700

    update vi language and ui

--- frontend/src/containers/profile/index.jsx
+++ frontend/src/containers/profile/index.jsx
@@ -1,4 +1,4 @@
-import { Button, CircularProgress, Container, Grid, TextField, Typography, Stack } from "@mui/material";
+import { Button, CircularProgress, Container, Grid, TextField, Typography, Stack, Divider, Paper } from "@mui/material";
 import React, { useEffect , useState} from "react";
 import { useForm, useWatch } from "react-hook-form";
 import { getStudentProfile, updateStudentProfile } from "../../apis/student";
@@ -39,76 +39,116 @@ export default function Profile() {
   if (loading)
     return <CircularProgress></CircularProgress>
   else
-  return <Container component = "form" onSubmit = {handleSubmit}>
-    <Typography sx = {{ mt: 2, mb: 3}} variant = "h2" align="center"> Student Profile</Typography>
-    <Grid container  rowSpacing={2} columnSpacing = {2} alignItems="center" justifyContent="flex-end">
-      <Grid item xs = {12} md = {2}>
-        <Typography variant = "h6" >First Name </Typography>
+  return (<Paper elevation={3} sx = {{mb : 2, mt: 2}}>
+    <Container component = "form"  onSubmit = {handleSubmit}>
+      <Typography  variant = "h2" sx = {{ p : 3}} align="center">
+        {/* Student Profile */}
+        Hồ sơ sinh viên 
+        </Typography>
+      <Divider/>
+      <Grid container  rowSpacing={2} columnSpacing = {2} alignItems="center" justifyContent="flex-end" sx = {{mt: 2}}>
+        <Grid item xs = {12} md = {2}>
+          <Typography variant = "h6" >
+            {/* First Name  */}
+            Tên 
+            </Typography>
+        </Grid>
+        <Grid item xs = {12} md = {4}>
+          <TextField label="First Name" variant="outlined" required defaultValue= {data.firstName} onChange={(event)=> handleChange(event, 'firstName')} fullWidth/>
+        </Grid>
+        <Grid item xs = {12} md = {2}>
+          <Typography variant = "h6">
+            {/* Last Name  */}
+            Họ
+            </Typography>
+        </Grid>
+        <Grid item xs = {12} md = {4}>
+          <TextField id="outlined-basic" label="Last Name" variant="outlined" required defaultValue= {data.lastName} onChange={(event)=> handleChange(event, 'lastName')} fullWidth/>
+        </Grid>
+        <Grid item xs = {12} md = {2}>
+          <Typography variant = "h6"> 
+          {/* Age */}
+          Tuổi 
+          </Typography>
+        </Grid>
+        <Grid item xs = {12} md = {3}>
+          <TextField id="outlined-basic" type = "number" label="Age" variant="outlined" defaultValue= {data.age} onChange={(event)=> handleChange(event, 'age')}  fullWidth/>
+        </Grid>
+        <Grid item xs = {12} md = {7}>
+          
+        </Grid>
+        <Grid item xs = {12} md = {2}>
+          <Typography variant = "h6">
+            {/* Address  */}
+            Địa chỉ 
+            </Typography>
+        </Grid>
+        <Grid item xs = {12} md = {10}>
+          <TextField id="outlined-basic" label="Address" variant="outlined" defaultValue= {data.address} onChange={(event)=> handleChange(event, 'address')} fullWidth/>
+        </Grid>
+        <Grid item xs = {12} md = {2}>
+          <Typography variant = "h6" > 
+          {/* Phone  */}
+          Số điện thoại 
+          </Typography>
+        </Grid>
+        <Grid item xs = {12} md = {10}>
+          <TextField id="outlined-basic" defaultValue= {data.phone} onChange={(event)=> handleChange(event, 'phone')}  label="Phone" variant="outlined" fullWidth/>
+        </Grid>
+        <Grid item xs = {12} md = {2}>
+          <Typography variant = "h6" >
+            Điểm trung bình /  CPA 
+            </Typography>
+        </Grid>
+        <Grid item xs = {12} md = {10}>
+          <TextField id="outlined-basic" label="CPA" step="any" InputProps={{ inputProps: { min: 0, max: 4 } }}  defaultValue= {data.CPA} onChange={(event)=> handleChange(event, 'CPA')} variant="outlined" fullWidth/>
+        </Grid>
+        <Grid item xs = {12} md = {2}>
+          <Typography variant = "h6" > 
+          {/* Available time  */}
+          Thời gian 
+          </Typography>
+        </Grid>
+        <Grid item xs = {12} md = {10}>
+          <TextField id="outlined-basic" label="Available time" variant="outlined"  defaultValue= {data.availableTime} onChange={(event)=> handleChange(event, 'availableTime')}  fullWidth/>
+        </Grid>
+        <Grid item xs = {12} md = {2}>
+          <Typography variant = "h6" > 
+          {/* Achievement  */}
+          Thành tích
+          </Typography>
+        </Grid>
+        <Grid item xs = {12} md = {10}>
+          <TextField multiline rows = {4} id="outlined-basic" label="Achievement" variant="outlined" defaultValue= {data.achievement} onChange={(event)=> handleChange(event, 'achievement')}  fullWidth/>
+        </Grid>
+        <Grid item xs = {12} md = {2}>
+          <Typography variant = "h6" > 
+          {/* Strength */}
+          Điểm mạnh 
+          </Typography>
+        </Grid>
+        <Grid item xs = {12} md = {10}>
+          <TextField multiline rows = {3} id="outlined-basic" label="Strength" defaultValue= {data.strength} onChange={(event)=> handleChange(event, 'strength')} variant="outlined" fullWidth/>
+        </Grid>
+        <Grid item xs = {12} md = {2}>
+          <Typography variant = "h6" > 
+          {/* Weakness  */}
+          Điểm yếu 
+          </Typography>
+        </Grid>
+        <Grid item xs = {12} md = {10}>
+          <TextField multiline rows = {3} id="outlined-basic"label="Weakness" defaultValue= {data.weakness} onChange={(event)=> handleChange(event, 'weakness')} variant="outlined" fullWidth/>
+        </Grid>
       </Grid>
-      <Grid item xs = {12} md = {4}>
-        <TextField label="First Name" variant="outlined" required defaultValue= {data.firstName} onChange={(event)=> handleChange(event, 'firstName')} fullWidth/>
-      </Grid>
-      <Grid item xs = {12} md = {2}>
-        <Typography variant = "h6">Last Name </Typography>
-      </Grid>
-      <Grid item xs = {12} md = {4}>
-        <TextField id="outlined-basic" label="Last Name" variant="outlined" required defaultValue= {data.lastName} onChange={(event)=> handleChange(event, 'lastName')} fullWidth/>
-      </Grid>
-      <Grid item xs = {12} md = {2}>
-        <Typography variant = "h6"> Age </Typography>
-      </Grid>
-      <Grid item xs = {12} md = {3}>
-        <TextField id="outlined-basic" type = "number" label="Age" variant="outlined" defaultValue= {data.age} onChange={(event)=> handleChange(event, 'age')}  fullWidth/>
-      </Grid>
-      <Grid item xs = {12} md = {7}>
-        
-      </Grid>
-      <Grid item xs = {12} md = {2}>
-        <Typography variant = "h6">Address </Typography>
-      </Grid>
-      <Grid item xs = {12} md = {10}>
-        <TextField id="outlined-basic" label="Address" variant="outlined" defaultValue= {data.address} onChange={(event)=> handleChange(event, 'address')} fullWidth/>
-      </Grid>
-      <Grid item xs = {12} md = {2}>
-        <Typography variant = "h6" > Phone </Typography>
-      </Grid>
-      <Grid item xs = {12} md = {10}>
-        <TextField id="outlined-basic" defaultValue= {data.phone} onChange={(event)=> handleChange(event, 'phone')}  label="Phone" variant="outlined" fullWidth/>
-      </Grid>
-      <Grid item xs = {12} md = {2}>
-        <Typography variant = "h6" > CPA </Typography>
-      </Grid>
-      <Grid item xs = {12} md = {10}>
-        <TextField id="outlined-basic" label="CPA" step="any" InputProps={{ inputProps: { min: 0, max: 4 } }}  defaultValue= {data.CPA} onChange={(event)=> handleChange(event, 'CPA')} variant="outlined" fullWidth/>
-      </Grid>
-      <Grid item xs = {12} md = {2}>
-        <Typography variant = "h6" > Available time </Typography>
-      </Grid>
-      <Grid item xs = {12} md = {10}>
-        <TextField id="outlined-basic" label="Available time" variant="outlined"  defaultValue= {data.availableTime} onChange={(event)=> handleChange(event, 'availableTime')}  fullWidth/>
-      </Grid>
-      <Grid item xs = {12} md = {2}>
-        <Typography variant = "h6" > Achievement </Typography>
-      </Grid>
-      <Grid item xs = {12} md = {10}>
-        <TextField multiline rows = {4} id="outlined-basic" label="Achievement" variant="outlined" defaultValue= {data.achievement} onChange={(event)=> handleChange(event, 'achievement')}  fullWidth/>
-      </Grid>
-      <Grid item xs = {12} md = {2}>
-        <Typography variant = "h6" > Strength </Typography>
-      </Grid>
-      <Grid item xs = {12} md = {10}>
-        <TextField multiline rows = {3} id="outlined-basic" label="Strength" defaultValue= {data.strength} onChange={(event)=> handleChange(event, 'strength')} variant="outlined" fullWidth/>
-      </Grid>
-      <Grid item xs = {12} md = {2}>
-        <Typography variant = "h6" > Weakness </Typography>
-      </Grid>
-      <Grid item xs = {12} md = {10}>
-        <TextField multiline rows = {3} id="outlined-basic"label="Weakness" defaultValue= {data.weakness} onChange={(event)=> handleChange(event, 'weakness')} variant="outlined" fullWidth/>
-      </Grid>
-    </Grid>
-    <Stack>
-    <Button sx ={{m : 4}} size = "medium" type = 'submit' variant = "contained"> Submit </Button>
+      <Stack>
+      <Button sx ={{m : 4}} size = "medium" type = 'submit' variant = "contained">
+        {/* Submit  */}
+        Hoàn thành 
+        </Button>
 
-    </Stack>
-  </Container>
+      </Stack>
+    </Container>
+  </Paper>)
+
+  
 }
